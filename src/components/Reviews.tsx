517b3547b02553005d64d3b62f2402a8359f03ff
EleasarP <p-eleasar@chronostep.com>
2025-05-19 18:24:34 +0800

    feat: added mobile responsiveness for home page

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -3,7 +3,7 @@ import { getAllReviews } from "@/utils/reviewUtils";
 import ReviewCard from "./ReviewCard";
 import "@/styles/pages/_reviews.scss";
 import { Masonry, useInfiniteLoader } from "masonic";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReviewDetailModal from "./ModalPopup2";
 
 const MasonryCard = ({ index, data, width } : {index: number, data: any, width: number} ) => (
@@ -17,15 +17,49 @@ const Reviews = () => {
   const reviews = getAllReviews();
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [data, setData] = useState<any>({})
+    const [width, setWidth] = useState(
+      typeof window !== "undefined" ? window.innerWidth : 0
+    );
+  
+    useEffect(() => {
+      window.addEventListener("load", () => {
+        if (typeof window !== "undefined") {
+          handleResize();
+        }
+      });
+      window.addEventListener("resize", () => {
+        handleResize();
+      });
+  
+      return () => {
+        window.removeEventListener("resize", handleResize);
+        window.removeEventListener("load", handleResize);
+      };
+    }, [data]);
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+  const getColumns = () => {
+    if (width >= 1024) {
+      return 4
+    }
+
+    if (width >= 768) {
+      return 3
+    }
+
+    return 2
+  }
   return (
-    <section className="!w-full reviews !bg-white z-30 !rounded-t-[40px]">
+    <section className="!w-full reviews !bg-white z-30 rounded-t-6 sm:rounded-t-10">
       <div className="reviews__container">
         <h2 className="reviews__title">Latest Reviews</h2>
         <p className="reviews__subtitle">
           See what others are saying about their dining experiences
         </p>
 
-        <Masonry items={reviews} render={ReviewCard} columnGutter={20} columnWidth={304} maxColumnCount={4} />
+        <Masonry items={reviews} render={ReviewCard} columnGutter={width > 767 ? 20 : 12} maxColumnWidth={304} columnCount={getColumns()} maxColumnCount={4} />
       </div>
     </section>
   );
