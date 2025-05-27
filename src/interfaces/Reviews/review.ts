afe023d1f29c4e082ff8830a52321859671027a5
b-jan-chronostep <b-jan@chronostep.com>
2025-05-27 11:36:19 +0800

    [FETCH]TP-4 - Homepage review show data from BE

---
+++ src/interfaces/Reviews/review.ts
@@ -0,0 +1,55 @@
+export interface ReviewedDataProps {
+  databasedId: number;
+  id: number;
+  reviewMainTitle: string;
+  content: string;
+  uri: string;
+  reviewStars: string;
+  date: string;
+  reviewImages: {
+    databaseId: number;
+    id: string;
+    sourceUrl: string;
+  }
+  author: {
+    name: string
+    node: {
+      id: string;
+      databaseId: number
+      name: string;
+      palates: string;
+      avatar: {
+        url: string;
+      }
+    }
+  }
+  commentedOn: {
+    node: {
+      databaseId: number;
+      title: string;
+      slug: string;
+      fieldMultiCheck90: string;
+      featuredImage: {
+        node: {
+          databaseId: string;
+          altText: string;
+          mediaItemUrl: string;
+          mimeType: string;
+          mediaType: string;
+          }
+      }
+    }
+  }
+}
+
+export interface ReviewCardProps {
+  data: ReviewedDataProps;
+  index: number
+  width: number
+}
+
+export interface ReviewModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  data: any;
+}
