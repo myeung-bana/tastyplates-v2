6bd50fe506cd55b7b84949c01e36373d9a08e817
Paul <e-paul@chronostep.com>
2025-06-19 19:17:46 +0800

    fix

--- src/app/listing/explanation/page.tsx
+++ src/app/listing/explanation/page.tsx
@@ -1,10 +1,13 @@
 import ListingExplanation from "@/components/Restaurant/Listing/ListingExplanation";
+import { Suspense } from "react";
 
 const ListingExplanationPage = () => {
   return (
-    <section>
-      <ListingExplanation />
-    </section>
+    <Suspense fallback={<div className="flex items-center justify-center min-h-screen">Loading...</div>}>
+      <section>
+        <ListingExplanation />
+      </section>
+    </Suspense>
   );
 };
 
