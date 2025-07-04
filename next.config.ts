f7fdc9176b7121d6d494b5a4528a9d2edc2ab5ae
Paul <e-paul@chronostep.com>
2025-07-04 11:06:59 +0800

    [ADD]TP-72 - add pwa

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
+
+const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig: NextConfig = {
   sassOptions: {
@@ -6,11 +9,32 @@ const nextConfig: NextConfig = {
     prependData: `@import "base/variables";`,
   },
   images: {
-    domains: [
-      "wordpress.test",
-      "localhost",
-      "secure.gravatar.com",
-      "www.tastyplates.co", // ✅ critical fix
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "wordpress.test",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "secure.gravatar.com",
+        pathname: "/avatar/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/a/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.tastyplates.co",
+        pathname: "/**",
+      },
     ],
   },
   env: {
@@ -18,4 +42,8 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
