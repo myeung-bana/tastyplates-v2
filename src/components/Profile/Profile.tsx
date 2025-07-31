889dcd529226f8c9696c778dd0d38d6ce714e8da
b-jan-chronostep <b-jan@chronostep.com>
2025-07-31 16:04:30 +0800

    TP-107-Profile user palate text color

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -759,7 +759,7 @@ const Profile = ({ targetUserId }: ProfileProps) => {
                         return (
                           <span
                             key={index}
-                            className="bg-[#f1f1f1] py-1 px-2 rounded-[50px] text-xs font-medium text-[#E36B00] flex items-center gap-1"
+                            className="bg-[#f1f1f1] py-1 px-2 rounded-[50px] text-xs font-medium text-[#31343f] flex items-center gap-1"
                           >
                             {flagSrc && (
                               <img
