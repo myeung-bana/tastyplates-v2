02e09e4e7034914b6facdc1186fa093ea1e3e46b
GerandPar <141900997+Symphonie234@users.noreply.github.com>
2025-08-01 16:05:28 +0800

    created pages for terms of service and privacy policy

---
+++ src/app/terms-of-service/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+export default function TermsOfService() {
+  return (
+    <>
+      <Navbar />
+      <main className="min-h-screen flex flex-col justify-between bg-white">
+        <div className="pt-24 px-4">
+          <h1 className="text-[24px] font-bold text-center text-[#31343F] mb-6">
+            Terms of Service
+          </h1>
+        </div>
+        <div className="flex-1 flex items-center justify-center px-4 pb-8">
+          <div className="w-full" style={{ maxWidth: "624px" }}>
+            <div className="text-[#31343F] flex flex-col gap-3 p-0">
+              <p className="text-[14px]">Last Updated: July 30, 2025</p>
+              <p className="text-[14px]">
+                Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis. Class
+                aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos.
+              </p>
+              <h2 className="font-bold text-[16px]">1. Who Control My Personal Information</h2>
+              <h3 className="font-bold text-[14px]">1.1 Controller</h3>
+              <p className="text-[14px]">
+                Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis. Class
+                aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos.
+              </p>
+              <h3 className="font-bold text-[14px]">1.2 Lorem Ipsum</h3>
+              <p className="text-[14px]">
+                Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis. Class
+                aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos.
+              </p>
+              <p className="text-[14px]">
+                Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis. Class
+                aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos.
+              </p>
+              <p className="text-[14px]">
+                Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis. Class
+                aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos.
+              </p>
+              <p className="text-[14px]">
+                Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis. Class
+                aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos.
+              </p>
+              <h3 className="font-bold text-[14px]">1.3 Lorem Ipsum</h3>
+              <p className="text-[14px]">
+                Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis. Class
+                aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos.
+              </p>
+              <h2 className="font-bold text-[16px]">2. Lorem ipsum dolor sit amet consectetur</h2>
+              <h3 className="font-bold text-[14px]">2.1 Lorem Ipsum</h3>
+              <p className="text-[14px]">
+                Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis. Class
+                aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos.
+              </p>
+              <h3 className="font-bold text-[14px]">2.2 Lorem Ipsum</h3>
+              <p className="text-[14px]">
+                Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis. Class
+                aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos.
+              </p>
+              <h3 className="font-bold text-[14px]">2.3 Lorem Ipsum</h3>
+              <p className="text-[14px]">
+                Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis. Class
+                aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos.
+              </p>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </main>
+    </>
+  );
+}
